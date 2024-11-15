0f8b115c724afa387bceca786d7a82f881b31554
Maya Eliyahu <mayaeliyahu12@gmail.com>
2024-11-16 01:59:59 +0200

    Add the submission project

--- ex1.c
+++ ex1.c
@@ -1,45 +1,86 @@
-/******************
-Name:
-ID:
-Assignment:
-*******************/
+/*******************
+Name: Maya Eliyahu.
+ID: .
+Assignment: ex1.
+********************/
+
 #include <stdio.h>
 
-// REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
+// REMINDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
+int main()
+{
+    /*
+    What bit:
+    Scan two integers (representing number and a position)
+    Print the bit in this position.
+    */
+    int num, position;
+    printf("What bit:\n");
+    printf("Please enter a number:\n");
+    scanf("%d", &num);
+    printf("Please enter a position:\n");
+    scanf("%d", &position);
+    printf("The bit in position %d of number %d is: %d\n", position, num, (num >> position) & 1);
 
-int main() {
-  
-  // What bit
-  printf("What bit:\n");
-  /*Scan two integers (representing number and a position)
-  Print the bit in this position. */
-  
-  // Set bit
-  printf("\nSet bit:\n");
-  /*Scan two integers (representing number and a position)
-  Make sure the bit in this position is "on" (equal to 1)
-  Print the output
-  Now make sure it's "off" (equal to 0)
-  Print the output */
+    /*
+    Set bit:
+    Scan two integers (representing number and a position)
+    Make sure the bit in this position is "on" (equal to 1)
+    Print the output
+    Now make sure it's "off" (equal to 0)
+    Print the output
+    */
+    printf("\nSet bit:\n");
+    printf("Please enter a number:\n");
+    scanf("%d", &num);
+    printf("Please enter a position:\n");
+    scanf("%d", &position);
+    num = num | (1 << position);
+    printf("Number with bit %d set to 1: %d\n", position, num);
+    num = num ^ (1 << position);
+    printf("Number with bit %d set to 0: %d\n", position, num);
 
-  // Toggle bit
-  printf("\nToggle bit:\n");
-  /*Scan two integers (representing number and a position)
-  Toggle the bit in this position
-  Print the new number */
-  
-  // Even - Odd
-  printf("\nEven - Odd:\n");
-  /* Scan an integer
-  If the number is even - print 1, else - print 0. */
-  
-  // 3, 5, 7, 11
-  printf("\n3, 5, 7, 11:\n");
-  /* Scan two integers in octal base
-  sum them up and print the result in hexadecimal base
-  Print only 4 bits, in positions: 3,5,7,11 in the result. */
+    /*
+    Toggle bit:
+    Scan two integers (representing number and a position)
+    Toggle the bit in this position
+    Print the new number
+    */
+    printf("\nToggle bit:\n");
+    printf("Please enter a number:\n");
+    scanf("%d", &num);
+    printf("Please enter a position:\n");
+    scanf("%d", &position);
+    num = num ^ (1 << position);
+    printf("Number with bit %d toggled: %d\n", position, num);
 
-  printf("Bye!\n");
-  
-  return 0;
+    /*
+    Even - Odd:
+    Scan an integer
+    If the number is even - print 1, else - print 0.
+    */
+    printf("\nEven - Odd:\n");
+    printf("Please enter a number:\n");
+    scanf("%d", &num);
+    printf("%d\n", (~num) & 1);
+
+    /*
+    3, 5, 7, 11:
+    Scan two integers in octal base
+    sum them up and print the result in hexadecimal base
+    Print only 4 bits, in positions: 3,5,7,11 in the result.
+    */
+    int num1, num2, sum;
+    printf("\n3, 5, 7, 11:\n");
+    printf("Please enter the first number (octal):\n");
+    scanf("%o", &num1);
+    printf("Please enter the second number (octal):\n");
+    scanf("%o", &num2);
+    sum = num1 + num2;
+    printf("The sum in hexadecimal: %X\n", sum);
+    printf("The 3,5,7,11 bits are: %d%d%d%d\n", (sum >> 3) & 1, (sum >> 5) & 1, (sum >> 7) & 1,(sum >> 11) & 1);
+
+    printf("Bye!\n");
+
+    return 0;
 }
